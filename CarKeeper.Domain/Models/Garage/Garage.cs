48588795e716ce2a35c67a03e49ed069e48f5792
Michael Lauritsen <i3028841@gmail.com>
2024-05-27 10:57:03 +0200

    added customers to garage

--- CarKeeper.Domain/Models/Garage/Garage.cs
+++ CarKeeper.Domain/Models/Garage/Garage.cs
@@ -1,14 +1,17 @@
-﻿using CarKeeper.Domain.Models.Vehicles;
+﻿using CarKeeper.Domain.Models.Owner;
+using CarKeeper.Domain.Models.Vehicles;
 
 namespace CarKeeper.Domain.Models.Garage;
 
 public class Garage
 {
     public List<Car> Cars { get; set; }
+    public List<VehicleOwner> Customers { get; set; }
 
     public Garage()
     {
         Cars = new List<Car>();
+        Customers = new List<VehicleOwner>();
     }
 
     public void AddCar(Car car)
@@ -19,5 +22,18 @@ public class Garage
     public void RemoveCar(Car car)
     {
         Cars.Remove(car);
+    }
+
+    public void AddCustomer(VehicleOwner customer)
+    {
+        if(customer is not null && !Customers.Contains(customer))
+        {
+            Customers.Add(customer);
+        }
+    }
+
+    public void RemoveCustomer(VehicleOwner customer)
+    {
+        Customers.Remove(customer);
     }
 }
--- CarKeeper.Test/Domain/GarageTests.cs
+++ CarKeeper.Test/Domain/GarageTests.cs
@@ -1,4 +1,5 @@
 ﻿using CarKeeper.Domain.Models.Garage;
+using CarKeeper.Domain.Models.Owner;
 using CarKeeper.Domain.Models.Vehicles;
 
 namespace CarKeeper.Test.Domain;
@@ -34,5 +35,71 @@ public class GarageTests
         garage.RemoveCar(car);
 
         Assert.DoesNotContain(car, garage.Cars);
+    }
+
+    [Fact]
+    public void CanAddCustomersToGarage()
+    {
+        //Arrange
+        var garage = new Garage();
+        var customer1 = new VehicleOwner("John", "doe@mail.com");
+        var customer2 = new VehicleOwner("Doe", "John@mail.com");
+
+        //Act
+        garage.AddCustomer(customer1);
+        garage.AddCustomer(customer2);
+
+        //Assert
+        Assert.Equal(2, garage.Customers.Count);
+        Assert.Contains(customer1, garage.Customers);
+        Assert.Contains(customer2, garage.Customers);
+    }
+
+    [Fact]
+    public void CanRemoveCustomerFromGarage()
+    {
+        //Arrange
+        var garage = new Garage();
+        var customer1 = new VehicleOwner("John", "doe@mail.com");
+        var customer2 = new VehicleOwner("Doe", "John@mail.com");
+        garage.AddCustomer(customer1);
+        garage.AddCustomer(customer2);
+
+        //Act
+        garage.RemoveCustomer(customer1);
+
+        //Assert
+        Assert.Single(garage.Customers);
+        Assert.DoesNotContain(customer1 , garage.Customers);
+        Assert.Contains(customer2, garage.Customers);
+    }
+
+    [Fact]
+    public void CantAddSameCustomerTwice()
+    {
+        //Arrange 
+        var garage = new Garage();
+        var customer = new VehicleOwner("John", "doe@mail.com");
+        garage.AddCustomer(customer);
+
+        //Act
+        garage.AddCustomer(customer);
+
+        //Assert
+        Assert.Single(garage.Customers);
+        Assert.Contains(customer, garage.Customers);
+    }
+
+    [Fact]
+    public void CannotAddNullCustomer()
+    {
+        //Arrange
+        var garage = new Garage();
+
+        //Act
+        garage.AddCustomer(null);
+
+        //Assert
+        Assert.Empty(garage.Customers);
     }
 }
